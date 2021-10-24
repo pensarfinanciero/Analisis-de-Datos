370de62084dc443898ce0d44da805782ee879cba
pensarfinanciero <78377285+pensarfinanciero@users.noreply.github.com>
2021-10-24 18:40:58 -0500

    JAGS _LAB 58 Corregido JSON en R
    Se corrige laboratorio, la Doctora nos compartió código para pasar a dataframe la base de datos de JSON

--- JAGS_LAB58/Inputs/Lab58script.R
+++ JAGS_LAB58/Inputs/Lab58script.R
@@ -1,36 +1,41 @@
-########################
-###LAB API DATAMEXICO###
-########################
-# Hecho con guto por Juan Antonio González Sierra
+##########################
+### LAB API DATAMEXICO ###
+##########################
 
-########################
-###LAB API DATAMEXICO###
-########################
+# Hecho con gusto por Juan Antonio González Sierra
 
-#Instala la paqueteria de jsonlite
+# Estudiante del Doctorado en Ciencias Económico Administrativas de la UAEH
+
+# Materia: Temas Selectos III Análisis de Datos
+
+# Docente: Carla Carolina Pérez Hernández
+
+# A continuación se va a trabajar con archivo en formato JSON que se descarga en la API de Data Mexico
+
+# Instala la paqueteria de jsonlite
 install.packages("jsonlite")
 
-#Llamar a la paqueteria
+# Llamar a la paqueteria
 library("jsonlite")
 
-#Buscar tu archivo descargado
+# Buscar tu archivo descargado en la computadora
 file.choose()
 
-#Traer el archivo
+# Traer el archivo al entorno de R
 datos_api2 <- fromJSON("C:\\Users\\jagon\\Desktop\\Doctorado\\4to semestre\\Modulo 2\\Laboratorios\\JAGS_LAB58 Uso de la API Data Mèxico\\JAGS_LAB58\\Inputs\\Municipality-Year-records.json")
+
+# Verificar como estan catalogados los datos (no es un dataframe)
 class(datos_api2)
 names(datos_api2)
 
+# Visualizar los datos que queremos de json
+View(datos_api2[["data"]])
 
-#Intento de pasarlo a Dataframe (no pude)
-datos_api2 <- lapply(datos_api2, function(x) {
-  x[sapply(x, is.null)] <- NA
-  unlist(x)
-})
+# Convertir en Dataframe los datos que se requieren
+final_API <- as.data.frame((datos_api2[["data"]]))
 
-#df<-as.data.frame(do.call("cbind", datos_api2))
-library(DT)
-datatable(head(df))
+# Comprobar que es un Dataframe
+class(final_API)
 
-#Bajo mi fracaso
-write_csv(df,"intentofracasodf.csv")
+# Para bajar el archivo en formato .csv
+write.csv(final_API, file = "final_API.CSV")
